783f1a8d1eafbe6fea247fec68db1945950b3c9a
sigidhanafi <sigidhanafi@gmail.com>
2023-01-14 23:05:46 +0700

    connect to mysql database

--- main.go
+++ main.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"golang-rest/handler"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+
+	// database connection
+	dsn := "root:@tcp(127.0.0.1:3306)/golangweb-gin?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Database connection failed")
+	}
+
+	log.Println("======Database Connection Success!======")
+
+	// register the router
 	router := gin.Default()
 
 	router.GET("/books", handler.GetBooks)
